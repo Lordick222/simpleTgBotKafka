80f99c44fbc53bd85d96db69eea90e0fe9dacff6
d.veselkov <d.veselkov@crpt.ru>
2021-09-04 18:37:45 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/demo/SimpleTgBotApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleTgBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleTgBotApplication>(*args)
+}
